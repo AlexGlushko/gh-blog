dc151998f9aa7f302b2b27b288eae68ba413869f
AlexGlushko <stalevar.1990@gmail.com>
2019-01-11 21:29:41 +0200

    Revert "Not working comments form"
    This reverts commit dbecfb01e201c7a83ed5f45ac6db7f5faf604c87.

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -18,23 +18,23 @@ class CategoryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Category::class);
     }
-/*
-    /**
-      * @return Category[] Returns an array of Category objects
-      */
-/*
-    public function articlesInCategory($value)
+
+    // /**
+    //  * @return Category[] Returns an array of Category objects
+    //  */
+    /*
+    public function findByExampleField($value)
     {
         return $this->createQueryBuilder('c')
-            ->join('App\Repository\ArticleRepository', 'a')
-            ->Where('a.category = :val')
+            ->andWhere('c.exampleField = :val')
             ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
             ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-*/
+    */
 
     /*
     public function findOneBySomeField($value): ?Category
